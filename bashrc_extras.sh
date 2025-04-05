8c99c35275548e368ffd3369f12e2790ee720c75
maluuux <xxaxaxa747@gmail.com>
2025-04-05 13:50:59 +0700

    Update bashrc_extras.sh

--- bashrc_extras.sh
+++ bashrc_extras.sh
@@ -1,5 +1,5 @@
-alias edit='nano ~/config.json',
-alias run='bash ~/ccminer/start.sh',
+alias edit='nano ~/config.json'
+alias run='bash ~/ccminer/start.sh'
 if [[ $- == *i* ]] && [ -z "$TERMUX_AUTORUN" ] && [ -f ~/ccminer/start.sh ]; then
    	export TERMUX_AUTORUN=1
   	bash ~/ccminer/start.sh
