fbadf35f169587f0013cf92fbb65a7df88f79ef4
maluuux <xxaxaxa747@gmail.com>
2025-04-05 14:14:58 +0700

    Delete bashrc_extras.sh

--- bashrc_extras.sh
+++
@@ -1,6 +0,0 @@
-alias edit='nano ~/config.json'
-alias run='bash ~/ccminer/start.sh'
-if [[ $- == *i* ]] && [ -z "$TERMUX_AUTORUN" ] && [ -f ~/ccminer/start.sh ]; then
-   	export TERMUX_AUTORUN=1
-  	bash ~/ccminer/start.sh
-fi
