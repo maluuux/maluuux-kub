83d0d3cb096d6adbe9d04ed7f3282234df272e3f
maluuux <xxaxaxa747@gmail.com>
2025-04-05 14:17:56 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-~/ccminer/ccminer -c
+echo '#!/bin/bash
+# ตรวจสอบก่อนรัน
+if [ ! -f ~/ccminer/config.json ]; then
+    echo "Error: config.json not found! Run edit_wallet.sh first."
+    exit 1
+fi
+
+~/ccminer/ccminer -c ~/ccminer/config.json
+' > ~/ccminer/run.sh
+chmod +x ~/ccminer/run.sh
